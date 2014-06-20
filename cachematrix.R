bf6f959d1a04264ccced6e60373d00a19d277cb2
ChiragNM <chirag.mandaviya@gmail.com>
2014-06-20 15:44:47 -0700

    Update cachematrix.R
    test the guthub usage

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Assignment 2
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
